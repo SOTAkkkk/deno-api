675d02ff518c5b63b6c5061dad3e6bd4b6e1e371
SOTAkkkk <>
2023-10-29 14:41:32 +0900

    ✨ create book server

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # deno-api
+
+## usage
+
+- run
+```shell
+deno run -A main.ts
+```
+
+- compile
+    - `-A`: 全許可
+    - `-o`: output先
+```shell
+deno compile -A -o ./dist/ main.ts
+```
+
+- compile(for windows環境)
+```shell
+deno compile -A --target x86_64-pc-windows-msvc --no-terminal -o ./dist/ main.ts
+```
--- main.ts
+++ main.ts
@@ -1,19 +1,29 @@
-import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import {Application, Router} from "https://deno.land/x/oak/mod.ts";
+
+const books = new Map<string, any>();
+books.set("1", {
+    id: "1",
+    title: "The Hound of the Baskervilles",
+    author: "Conan Doyle, Arthur",
+})
 
 const router = new Router();
-router.get("/", (ctx) => {
-    ctx.response.body = `<!DOCTYPE html>
-    <html>
-      <head><title>Hello oak!</title><head>
-      <body>
-        <h1>Hello oak!</h1>
-      </body>
-    </html>
-  `;
-});
+router.get("/", (context) => {
+    context.response.body = `Hello oak!`;
+    console.log("hello oak!")
+})
+    .get("/book", (context) => {
+        context.response.body = Array.from(books.values());
+    })
+    .get("/book/:id", (context) => {
+            if (books.has(context?.params?.id)) {
+                context.response.body = books.get(context.params.id);
+            }
+        }
+    );
 
 const app = new Application();
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-app.listen({ port: 8080 });
+app.listen({port: 8080});
